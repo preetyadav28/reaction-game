44de3b421e1cc8ee6d820dd981bc9c91c02bca32
preetyadav28 <83236414+preetyadav28@users.noreply.github.com>
2025-04-25 13:38:04 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,22 @@
+<script setup lang="ts">
+  import Container from  './components/Container.vue';
+</script>
 
+<template>
+  <Container />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
