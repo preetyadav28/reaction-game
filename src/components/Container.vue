5110d368feec1575efb95573b9e567d0df19b824
preetyadav28 <83236414+preetyadav28@users.noreply.github.com>
2025-04-25 13:39:05 +0530

    Add files via upload

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,1 +1,47 @@
+<template>
+  <div>
+    <h2>Welcome to the Reaction Game</h2>
+    <button @click='openModal'>Play</button>
+    <ReactionModal v-if="showModal" @close="closeModal" />
+    <template v-if='difference'>
+      <h3>{{ difference }} ms</h3>
+      <h2>{{ result }}</h2>
+    </template>
+  </div>
+</template>
 
+<script setup>
+  import { ref, computed } from 'vue';
+  import ReactionModal from './ReactionModal.vue'
+  
+  const showModal = ref(false);
+  let openedAt = ref(0);
+  let closedAt = ref(0);
+  const difference = computed(() => {
+    if(closedAt.value && openedAt.value){
+      return closedAt.value - openedAt.value
+    }
+    return 0
+  })
+  const result = computed(() => {
+    if(difference.value < 200) return 'Lightning ⚡'
+    if(difference.value < 299) return 'Cheetah 🐆'
+    if(difference.value < 399) return 'Average 🐾'
+    if(difference.value < 499) return 'Tortoise 🐢'
+    if(difference.value < 699) return 'Snail 🐌'
+    if(difference.value > 700) return 'Too Slow 😕'
+  })
+  console.log('result', result)
+  function openModal() {
+      const randomNumber = Math.floor(Math.random() * 3) + 1
+      setTimeout(() => {
+        openedAt.value = Date.now()
+        closedAt.value = 0
+        showModal.value = true
+      }, randomNumber * 1000)
+  }
+  function closeModal() {
+     closedAt.value = Date.now()
+     showModal.value = false
+  }
+</script>
---
+++ src/components/ReactionModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="overlay" @click="closeModal">
+    <div class="modal">
+      <h2>Click Me</h2>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const emit = defineEmits()
+  function closeModal() {
+    emit('close')
+  }
+</script>
+
+<style scoped>
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width:100vw;
+    height: 100vh;
+    background-color: rgba(0,0,0,0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .modal {
+    width:40%;
+    height:40%;
+    background-color: rgb(200,200,0);
+    border-radius:5%;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+  }
+</style>
